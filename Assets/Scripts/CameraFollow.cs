4e42f3297a3d863395032c8a651039a77e717d45
Viktor Nagymathe <nagymathe.viktor01@gmail.com>
2021-12-03 20:21:58 +0100

    camera now will follow player

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+	public Transform target;
+
+	public float smoothSpeed = 0.125f;
+	public Vector3 offset;
+
+	private void Start()
+	{
+		target = GameObject.FindWithTag("Player").transform;
+	}
+	void LateUpdate()
+	{
+		Vector3 desiredPosition = target.position + offset;
+		Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+		transform.position = smoothedPosition;
+
+		//transform.LookAt(target);
+	}
+}
